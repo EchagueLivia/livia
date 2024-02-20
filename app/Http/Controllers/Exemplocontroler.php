0061fed66c790047468dc5ec00ccbb06089960eb
unknown <livia.echague@estudate.ifms.edu.br>
2024-02-20 14:47:03 -0400

    iniciando

---
+++ app/Http/Controllers/Exemplocontroler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Exemplocontroler extends Controller
+{
+    //
+}
+Router::get('exemplo', 'App\https\Controllers\Exemplocontroler@index');
---
+++ resources/views/exemplo.blade.php
@@ -0,0 +1,9 @@
+<hmtl>
+    <head>
+    <title>exemplo</title>
+</head>
+<body>
+    <h1> exemplo </h1>
+    <p>oooiii</p>
+</body>
+</html>
